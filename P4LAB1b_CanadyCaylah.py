b3016f54126c74a77f2fd97ce7aefa9940cfb32c
Murrayc8545 <145588768+Murrayc8545@users.noreply.github.com>
2023-11-08 11:46:12 -0500

    Add files via upload

---
+++ P4LAB1b_CanadyCaylah.py
@@ -0,0 +1,27 @@
+import turtle            
+
+jay = turtle.Screen()
+cay = turtle.Turtle()
+cay.penup()
+
+    
+
+                         #draw straight line
+cay.goto(-30,50)         #centering in the screen
+cay.pendown()
+cay.pensize(3)
+cay.pencolor("red")     #color 
+cay.right(180)
+cay.circle(50,180)
+cay.penup()             #start next initial
+cay.forward(100)
+cay.goto(25,55)
+cay.pendown()
+cay.right(180)
+cay.circle(50,180)
+
+
+
+# end commands
+         
+
